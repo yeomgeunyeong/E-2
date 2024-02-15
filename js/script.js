8787dc19adb24ff4c1417e906bbe12a9ccd14536
yeomgeunyeong <yyynyeong@gmail.com>
2024-02-15 12:02:03 +0900

    02-15

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 $(function(){
 
+    $(".main > li").hover(function(){
+        $(this).find($(".sub")).stop().slideDown();
+    }, function(){
+        $(".sub").stop().slideUp();
+    })
 
+
+
+    setInterval(function(){
+        $(".top_move").animate({top:"-100%"}, 500, function(){
+            $(".top_move").css({top:"0"});
+            $(".top_move").append($(".top_move li").first());
+        })
+    }, 3000)
+
+
+    $(".p_click").click(function(){
+        $(".modal, .pop").show();
+    })
+
+    $(".close").click(function(){
+        $(".modal, .pop").hide();
+    })
 })//
